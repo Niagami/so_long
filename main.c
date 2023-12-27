b6c0b068538ff06882f2d34b8475d3f08fa98daf
Jason Teste <jteste@k2r1p10.42mulhouse.fr>
2023-12-27 17:13:19 +0100

    ok

---
+++ main.c
@@ -0,0 +1,63 @@
+// -----------------------------------------------------------------------------
+// Codam Coding College, Amsterdam @ 2022-2023 by W2Wizard.
+// See README in the root project for more information.
+// -----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "./MLX42/include/MLX42/MLX42.h"
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+static mlx_image_t* image;
+
+void ft_hook(void* param)
+{
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_W))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_S))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_A))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_D))
+		image->instances[0].x += 5;
+}
+
+int32_t main(int32_t argc, const char* argv[])
+{
+	mlx_t* mlx;
+
+	// Gotta error check this stuff
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "so_long", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	mlx_texture_t* texture = mlx_load_png("./img/test.png");
+	if (!texture)
+        error();
+	if (!(image = mlx_texture_to_image(mlx, texture)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
